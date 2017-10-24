cad156be8dd2dd1c472809284a675275a36c49da
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-10-24 19:50:05 -0400

    updated files with solutions to review

--- practice_API/index.js
+++ practice_API/index.js
@@ -1,19 +1,77 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+let express = require('express')
+let bodyParser = require('body-parser')
 
-const app = express()
+// Create our app and parser
+let app = express()
+let parser = bodyParser.json()
 
-app.use(bodyParser.json)
+// Wire up the body parser
+app.use(parser)
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+// Products dictionary
+let productsDict = {}
+let id = 0
+
+// This is our status route
+app.get('/status', (request, response) => {
+    response.send('{"status": "on" }')
 })
 
-// create a route that returns a json object that is
-// simply this { 'status' : 'on'}
-// the route should be /status
-// the verb should be a GET
+// Gets all products
+app.get('/products', (request, response) => {
+    let arrayOfProducts = []
+    for (key in productsDict) {
+        arrayOfProducts.push(productsDict[key])
+    }
+    response.send(arrayOfProducts);
+})
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+// Creates a new product
+app.post('/products', (request, response) => {
+    // Get the new product from the request body
+    let newProduct = request.body;
+
+    // Generate/set the new ID
+    newProduct.id = id;
+    productsDict[id] = newProduct;
+
+    // Increment ID for the next post
+    id++;
+
+    // Send back the new product
+    response.json(newProduct); 
+})
+
+// Updates a target product
+app.put('/products/:id', (request, response) => {
+    // Get the updated product
+    let updatedProduct = request.body
+    // We need to convert the parameter to a number.
+    // URL params are always and forever strings.
+    let id = Number(request.params.id)
+    
+    // Check to see if the product exists. If it doesn't, 
+    // we want to return a 404 Not Found.
+    if (productsDict[id]) {
+        updatedProduct.id = id 
+        productsDict[id] = updatedProduct
+        response.json(updatedProduct)
+    } else {
+        response.status(404).send('Not found');
+    }
+})
+
+// Deletes a target product
+app.delete('/products/:id', (request, response) => {
+    if (productsDict[id]) {
+        delete productsDict[id]
+        response.status(204).send()
+    } else {
+        response.status(404).send('Not found')
+    }
+})
+
+// tell the app which port to listen on
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
 })
